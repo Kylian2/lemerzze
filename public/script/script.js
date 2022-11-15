7104d174e65013076813981ca96ed57d7960a956
Kylian2 <86042198+Kylian2@users.noreply.github.com>
2022-11-15 16:24:57 +0100

    Delete public directory

--- public/script/script.js
+++
@@ -1,8 +0,0 @@
-const compte = document.getElementById('compte');
-const menu = document.getElementById('menu');
-const classInactif = 'menu-inactif';
-
-compte.onclick = function () {
-    console.log('click');
-    menu.classList.toggle('menu-inactif');
-};
